34cede6336e84dc1ea30649b252d85a48d2ec46a
Jackie Chen <jackychen3010@gmail.com>
2018-03-01 10:38:09 -0800

    thirdpartyAPl lab

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,16 @@
 function initMap() {
 	// add your code here
+	var ucsd_ltlng = {lat:32.87879, lng:-117.23602};
+
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: ucsd_ltlng, 
+		zoom: 15
+	});
+
+	var market = new google.maps.Marker({
+		position: ucsd_ltlng,
+		map: map,
+		title: "UCSD"
+	});
+
 }
